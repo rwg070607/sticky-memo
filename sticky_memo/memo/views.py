c99f13fbc62ae79cfff8230fd6d51e2d7b6ea55e
rwg070607 <133008024+rwg070607@users.noreply.github.com>
2023-06-15 11:57:37 +0000

    세번째 백엔드 수업

--- sticky_memo/memo/views.py
+++ sticky_memo/memo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from memo.models import Memo
 
 def get_all_memo(request):
@@ -6,3 +6,23 @@ def get_all_memo(request):
     context = {"memos": memo_list}
 
     return render(request, "memo_list.html", context)
+
+def get_memo(request, pk):
+    memo = Memo.objects.get(id=pk)
+    context = {'memo':memo}
+
+    return render(request, 'memo_detail.html', context)
+
+from memo.forms import MemoForm
+def create_memo(request):
+    if request.method == 'GET':
+        form = MemoForm()
+        context = {'form': form}
+        return render(request, 'memo_create.html', context)
+    
+    elif request.method == 'POST':
+        form = MemoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('memo_list')
+    
